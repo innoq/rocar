3c1fa42b7ee589002d6f8677d346643531e4ef6e
FND <fnd@localhost.localdomain>
2015-06-30 10:47:29 +0200

    created Flask application
    still static

---
+++ rocar/web.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def frontpage():
+    return redirect(url_for("catalog"))
+
+
+@app.route("/catalog")
+def catalog():
+    return render("catalog.html")
+
+
+def render(template, *args, **kwargs):
+    kwargs["styles"] = (url_for("static", filename="styles/%s" % name)
+            for name in ["layout.css", "main.css"])
+    return render_template(template, *args, **kwargs)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
