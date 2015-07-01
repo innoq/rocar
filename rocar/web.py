238c3f3d79cff070df07c5365363eedb192b38d5
FND <fnd@localhost.localdomain>
2015-07-01 12:44:23 +0200

    implemented location selection

--- rocar/web.py
+++ rocar/web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 from . import store
 
@@ -13,10 +13,23 @@ def frontpage():
 
 @app.route("/catalog")
 def catalog():
-    return render("catalog.html", locations=store.locations)
+    selected_locations = set(request.args.getlist("location"))
+    locations = [Location(id, name, id in selected_locations)
+            for id, name in store.locations.items()]
+    locations.sort(key=lambda l: l.selected, reverse=True)
+
+    return render("catalog.html", locations=locations)
 
 
 def render(template, *args, **kwargs):
     kwargs["styles"] = (url_for("static", filename="styles/%s" % name)
             for name in ["layout.css", "main.css"])
     return render_template(template, *args, **kwargs)
+
+
+class Location:
+
+    def __init__(self, id, name, selected=False):
+        self.id = id
+        self.name = name
+        self.selected = selected
