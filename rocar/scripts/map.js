d76613e80c35472f47c24cb9491d48af3dae0e52
FND <fnd@localhost.localdomain>
2015-07-08 09:54:21 +0200

    fixed map dimensions on Chrome
    yay simplification

--- rocar/scripts/map.js
+++ rocar/scripts/map.js
@@ -2,7 +2,6 @@
 "use strict";
 
 var leaflet = require("leaflet/dist/leaflet-src");
-var dom = require("uitil/dom");
 
 leaflet.Icon.Default.imagePath = "/static/vendor/leaflet_images"; // FIXME: hard-coded
 
@@ -10,13 +9,7 @@ module.exports = function(container) {
 	var coordinates = extractCoordinates(container);
 	var origin = coordinates[0] || [40, 5];
 
-	// wrapper ensures consistent size -- XXX: should not be necessary!?
-	var wrapper = document.createElement("div");
-	wrapper.style.width = "100%"; // XXX: insufficiently generic?
-	wrapper.style.height = "100%"; // XXX: insufficiently generic?
-	dom.replaceNode(container, wrapper);
-
-	var map = leaflet.map(wrapper);
+	var map = leaflet.map(container);
 	leaflet.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
